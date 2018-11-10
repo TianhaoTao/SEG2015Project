b771450d5ed75a55a8625f1e037785955cb76877
zhikai wang <shanekid@msn.cn>
2018-11-10 14:08:24 -0500

    UserTest

---
+++ app/src/test/java/com/example/tianhao/seg2105project/UserTest.java
@@ -0,0 +1,20 @@
+package com.example.tianhao.seg2105project;
+
+import com.example.tianhao.seg2105project.Model.User;
+
+        import static org.junit.Assert.*;
+        import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void checkUserName() {
+        User user = new User("username", "email", "password", "userType");
+        assertEquals("check the name of user", "username", user.getUsername());
+    }
+
+
+    public void checkEmail() {
+        User user = new User("username", "email", "password", "userType");
+        assertEquals("check the email of user", "email", user.getEmail());
+    }
+}

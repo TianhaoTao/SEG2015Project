da2140ba0b652b6a4d4ebcdc0b6128a214e0405f
Tianhao Tao <ttao061@uottawa.ca>
2018-11-17 20:00:08 -0500

    the back button in AddServiceToProfile is available now

--- app/src/main/java/com/example/tianhao/seg2105project/AddServiceToProfile.java
+++ app/src/main/java/com/example/tianhao/seg2105project/AddServiceToProfile.java
@@ -2,12 +2,31 @@ package com.example.tianhao.seg2105project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.tianhao.seg2105project.Model.Application;
 
 public class AddServiceToProfile extends AppCompatActivity {
+
+    Button buttonDelete, buttonGOBACK, buttonSave;
+    private Application application = Application.getInstance(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service_to_profile);
+
+
+        buttonDelete = (Button)findViewById(R.id.delete_service);
+        buttonSave = (Button) findViewById(R.id.save_button);
+        buttonGOBACK = (Button) findViewById(R.id.cancel);
+
+        buttonGOBACK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }

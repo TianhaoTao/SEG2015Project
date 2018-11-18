4318138498799d12e0a4b7350765d4d5930023c0
Tianhao Tao <ttao061@uottawa.ca>
2018-11-18 17:07:01 -0500

    service provider

--- app/src/main/java/com/example/tianhao/seg2105project/Model/ServiceProvider.java
+++ app/src/main/java/com/example/tianhao/seg2105project/Model/ServiceProvider.java
@@ -1,4 +1,47 @@
 package com.example.tianhao.seg2105project.Model;
 
 public class ServiceProvider extends User {
+
+    private String address, phone, companyName, description;
+    private boolean hasLicensed;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isHasLicensed() {
+        return hasLicensed;
+    }
+
+    public void setHasLicensed(boolean hasLicensed) {
+        this.hasLicensed = hasLicensed;
+    }
 }
--- app/src/main/java/com/example/tianhao/seg2105project/profileFragment.java
+++ app/src/main/java/com/example/tianhao/seg2105project/profileFragment.java
@@ -3,6 +3,7 @@ package com.example.tianhao.seg2105project;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,7 +30,16 @@ public class profileFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_profile, container, false);
 
+        recyclerView = view.findViewById(R.id.recycler_view);
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();//refresh the recylerview every time it is brought to the front
+        application = Application.getInstance(getActivity());
+        recyclerView.setAdapter(new ServiceViewAdapter(getActivity(),application.getServiceArrayList()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 }

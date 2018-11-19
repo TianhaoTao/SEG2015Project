bef130c856b71241daa7c0d41890a9c0e498d261
Xiaohan Yu <xyu095@uottawa.ca>
2018-11-18 20:34:45 -0500

    Service Provider Profile

--- app/src/main/java/com/example/tianhao/seg2105project/ServiceProviderProfile.java
+++ app/src/main/java/com/example/tianhao/seg2105project/ServiceProviderProfile.java
@@ -1,13 +1,51 @@
 package com.example.tianhao.seg2105project;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ServiceProviderProfile extends AppCompatActivity {
+
+    TextInputLayout editAddress;
+    TextInputLayout editPhone;
+    TextInputLayout editCompany;
+    TextInputLayout editDescription;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button ButtonSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_profile);
+
+        editAddress = findViewById(R.id.editAddress);
+        editPhone = findViewById(R.id.editPhone);
+        editCompany = findViewById(R.id.editCompany);
+        editDescription = findViewById(R.id.editDescription);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        ButtonSubmit = (Button)findViewById(R.id.buttonSubmit2);
+
+        ButtonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioButtonId = radioGroup.getCheckedRadioButtonId();
+                radioButton = (RadioButton) findViewById(radioButtonId);
+
+            }
+        });
+
+    }
+
+    public void checkButton(View v){
+        int radioButtonId = radioGroup.getCheckedRadioButtonId();
+        radioButton = (RadioButton) findViewById(radioButtonId);
     }
 }

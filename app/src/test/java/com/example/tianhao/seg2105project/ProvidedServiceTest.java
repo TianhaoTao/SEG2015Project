f034aa7f99375a1ed46183aebd3111122846b695
WowkieLiu <gracethedeveloper@gmail.com>
2018-12-04 22:16:51 -0500

    unit test

--- app/src/test/java/com/example/tianhao/seg2105project/ProvidedServiceTest.java
+++ app/src/test/java/com/example/tianhao/seg2105project/ProvidedServiceTest.java
@@ -13,26 +13,32 @@ public class ProvidedServiceTest {
 
     @Test
     public void checkID(){
-        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12", 5);
         assertEquals("check the ID of provided service", "123456", service.getId());
     }
 
     @Test
     public void checkService(){
-        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12", 5);
         assertEquals("check the service of provided service", service1, service.getService());
     }
 
     @Test
     public void checkServiceProviderName(){
-        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12", 5);
         assertEquals("check the name of service provider", "hhh", service.getServiceProviderName());
     }
 
     @Test
     public void checkTimeSlots(){
-        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12", 5);
         assertEquals("check the time slots of provided service", "12", service.getTimeSlots());
+    }
+
+    @Test
+    public void checkRate(){
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12", 5);
+        assertEquals("check the rate of provided service", 5, service.getRate(), 0.001);
     }
 
 

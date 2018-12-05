9f485b52420c8d00759926a642946ac1c9f94f52
WowkieLiu <gracethedeveloper@gmail.com>
2018-12-04 21:46:11 -0500

    unit test

---
+++ app/src/test/java/com/example/tianhao/seg2105project/ProvidedServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.tianhao.seg2105project;
+
+
+import com.example.tianhao.seg2105project.Model.ProvidedService;
+import com.example.tianhao.seg2105project.Model.Service;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ProvidedServiceTest {
+
+    Service service1 = new Service("hhh", 12);
+
+    @Test
+    public void checkID(){
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        assertEquals("check the ID of provided service", "123456", service.getId());
+    }
+
+    @Test
+    public void checkService(){
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        assertEquals("check the service of provided service", service1, service.getService());
+    }
+
+    @Test
+    public void checkServiceProviderName(){
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        assertEquals("check the name of service provider", "hhh", service.getServiceProviderName());
+    }
+
+    @Test
+    public void checkTimeSlots(){
+        ProvidedService service = new ProvidedService("123456", service1, "hhh", "12");
+        assertEquals("check the time slots of provided service", "12", service.getTimeSlots());
+    }
+
+
+
+
+
+}
